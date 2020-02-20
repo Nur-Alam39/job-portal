abfb5c61aacacee6f18a1ff0496cdd6c52180299
Nur-E-Alam Jony <nuralam.cse.bsmrstu@gmail.com>
2020-02-21 01:27:30 +0600

    extended navbar file

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,4 @@
+@extends('navbar')
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,41 +8,17 @@
         <title>Opportunity</title>
         <link rel="icon" href="icon.png" type="image/icon type">
 
-        <!--Bootstrap 4-->
-        <script src="public/bootstrap4/js/bootstrap.js"></script>
-        <link href="public/bootstrap4/css/bootstrap.css" rel="stylesheet">
-
-        <script src="{{asset('public/bootstrap4/js/bootstrap.js')}}"></script>
-        <link href="{{asset('public/bootstrap4/css/bootstrap.css')}}" rel="stylesheet">
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/fe6cc37c91.js" crossorigin="anonymous"></script>
-
         <style type="text/css">
-          body
-          {
-            background-color: rgb(253, 253, 253); 
-          }
-          h4 {
+          h5 {
 			    position: relative;
 			}
 
-			h4 span {
-			    background-color: rgb(253, 253, 253); 
+			h5 span {
+			    background-color: #ffffff;
 			    padding-right: 10px;
 			}
 
-			h4:after {
+			h5:after {
 			    content:"";
 			    position: absolute;
 			    bottom: 0;
@@ -55,27 +32,6 @@
 
     </head>
     <body>
-        <!-- Navigation -->
-          <nav class="navbar fixed-top navbar-expand-lg shadow-sm p-3 mb-5 bg-white rounded navbar-light bg-white font-weight-normal">
-            <a class="navbar-brand" href="#">Opportunity</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                  <li class="nav-item">
-                      <a class="nav-link" href="{{url('/')}}">Find Jobs</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="{{url('/')}}">Post Job</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="{{url('/about')}}">Profile</a>
-                    </li>
-              </ul>
-            </div>
-        </nav>
-
         <div class="row" style="margin: 0; margin-top: 8%">
           <div class="col-lg-3">
           	<div class="">
@@ -103,8 +59,8 @@
           </div>
           <div class="col-lg-9">
           	
-          	<h4><span>Experience</span></h4>
-          	<h4 class="mt-4"><span>Education</span></h4>
+          	<h5><span>Experience</span></h5>
+          	<h5 class="mt-4"><span>Education</span></h5>
           	<li>
           		BSc - Bangabandhu Sheikh Mujibur Rahman Science & Technology University - Gopalganj</br>
           		&nbsp; &nbsp;&nbsp;&nbsp;Computer Science and Engineering
@@ -120,18 +76,18 @@
           		&nbsp; &nbsp;&nbsp;&nbsp;Science
           		Passing Year: 2012 GPA-5.00
           	</li>
-          	<h4 class="mt-4"><span>Skill</span></h4>
+          	<h5 class="mt-4"><span>Skill</span></h5>
   
           	<li>Language: C++</li>
           	<li>Web: PHP, MySQL</li>
           	<li>Framework: Laravel</li>
 
-          	<h4 class="mt-4"><span>Project</span></h4>
-          	<h4 class="mt-4"><span>Training</span></h4>
-          	<h4 class="mt-4"><span>Publication</span></h4>
-  			<h4 class="mt-4"><span>Additional Activities</span></h4>
-  			<h4 class="mt-4"><span>Extra Cirriculum Activities</span></h4>
-  			<h4 class="mt-4"><span>Reference</span></h4>
+          	<h5 class="mt-4"><span>Project</span></h5>
+          	<h5 class="mt-4"><span>Training</span></h5>
+          	<h5 class="mt-4"><span>Publication</span></h5>
+  			<h5 class="mt-4"><span>Additional Activities</span></h5>
+  			<h5 class="mt-4"><span>Extra Cirriculum Activities</span></h5>
+  			<h5 class="mt-4"><span>Reference</span></h5>
           </div>
         </div>
     </body>
