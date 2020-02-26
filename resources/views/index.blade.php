f561b56b0423a6aecdd19653ddab6935ba7ed769
Nur-E-Alam Jony <nuralam.cse.bsmrstu@gmail.com>
2020-02-26 15:20:38 +0600

    added update and delete operartions for job post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
-@extends('navbar')
+@extends('layout.app')
+@section('content')
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,16 +16,51 @@
           a.card, a.card:hover {
             color: inherit;
           }
+          .dropdown:hover>.dropdown-menu {
+            display: block;
+          }
+
+          .dropdown>.dropdown-toggle:active {
+            pointer-events: none;
+          }
+          #inputEmail4, #inputPassword4
+          {
+            border:none;
+            border-bottom:solid 1px #cccccc;
+            border-radius: 0px;
+          }
         </style>
 
     </head>
     <body>
 
-        <div class="container-fluid p-2" style="margin-top: 4%">
-          <div class="row" style="margin: 0;">
-           <h3 class='p-4'>Index Page</h3>
+        <div class="container-fluid p-2" style="margin-top: 6%">
+          <div class="container-fluid" style="padding: 3%; width: 80%">
+            <div class="card" style="border:none; background-color:transparent ">
+            <div class="card-body">
+              <div class="form-row">
+                <div class="form-group col-md-8">
+                  <h5>Find Your Opportunity</h5>
+                </div>
+              </div>
+               <div class="form-row" style="">
+                <div class="form-group col-md-6">
+                  <input type="email" class="form-control" id="inputEmail4" placeholder="Job title">
+                </div>
+                <div class="form-group col-md-4">
+                  <input type="password" class="form-control" id="inputPassword4" placeholder="Location">
+                </div>
+                <div class="form-group col-md-2">
+                  <button type="submit" class="btn btn-primary mb-2 form-control">Search</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div class="container-fluid" style="background-color:; padding: 2%">
+           
           </div>
         </div>
-
     </body>
 </html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,17 @@
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/jobs', function () {
+/*Route::get('/jobs', function () {
     return view('jobs');
-});
+});*/
 Route::get('/profile', function () {
     return view('profile');
 });
 
+Route::get('/jobs', 'JobController@index');
+Route::get('/jobs/create', 'JobController@create');
+Route::post('/jobs/store', 'JobController@store');
+Route::get('/jobs/show/{job_id}', 'JobController@show');
+Route::get('/jobs/edit/{job_id}', 'JobController@edit');
+Route::post('/jobs/update/{job_id}', 'JobController@update');
+Route::get('/jobs/delete/{job_id}', 'JobController@destroy');
