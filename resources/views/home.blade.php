47c0bbba03bdac5853a865889c4767da3ce5bda3
Nur-E-Alam Jony <nuralam.cse.bsmrstu@gmail.com>
2020-02-29 00:41:23 +0600

    added user authentication

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container"  style="margin: 8%">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
